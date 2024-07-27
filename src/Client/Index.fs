51289174bca660a4a7503c4c5e729cc726a23370
egray <evangray7373@gmail.com>
2024-07-27 16:32:37 -0500

    Working on dashboard

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -97,31 +97,117 @@ module ViewComponents =
             ]
         ]
 
+    let dailyTargetsSidebar model dispatch =
+        Html.div [
+            prop.style [
+                style.borderWidth 1
+                style.borderColor "black"
+            ]
+        ]
+
 let view model dispatch =
-    Html.section [
-        prop.className "h-screen w-screen"
+    Html.div [
+        prop.className "h-screen w-screen flex flex-col"
         prop.style [
             style.backgroundSize "cover"
-            style.backgroundImageUrl "https://unsplash.it/1200/900?random"
+            style.backgroundColor "#C0E5C8"
             style.backgroundPosition "no-repeat center center fixed"
+            style.margin 0
         ]
 
         prop.children [
-            Html.a [
-                prop.href "https://safe-stack.github.io/"
-                prop.className "absolute block ml-12 h-12 w-12 bg-teal-300 hover:cursor-pointer hover:bg-teal-400"
-                prop.children [ Html.img [ prop.src "/favicon.png"; prop.alt "Logo" ] ]
-            ]
-
             Html.div [
-                prop.className "flex flex-col items-center justify-center h-full"
+                prop.children [
+                     Html.h1 [
+                        prop.className "text-center text-5xl font-bold mb-3 rounded-md p-4"
+                        prop.text "NutritionCalculator"
+                        prop.style [ style.color "#16302B" ]
+                    ]
+                ]
+            ]
+            Html.div [
+                prop.className "mx-[100px] mt-[10px]"
+                prop.style [
+                    style.border (length.px 1, borderStyle.solid, "black")
+                ]
+                prop.text "top bar"
+            ]
+            Html.div [
+                prop.className "mx-[100px] mb-[75px] mt-[10px]"
+                prop.style [
+                    style.flexGrow 1
+                    style.border (length.px 1, borderStyle.solid, "black")
+                ]
+                prop.children [
+                    Html.div [
+                        prop.className "grid grid-cols-3 grid-rows-3 gap-4"
+                        prop.children [
+                            Html.div [
+                                prop.className "flex items-center justify-center"
+                                prop.style [style.backgroundColor "gray"]
+                                prop.children [
+                                    Html.p [
+                                        prop.text 1
+                                    ]
+                                ]
+                            ]
+                            Html.div [
+                                prop.className "flex items-center justify-center"
+                                prop.style [style.backgroundColor "gray"]
+                                prop.children [
+                                    Html.p [
+                                        prop.text 2
+                                    ]
+                                ]
+                            ]
+                            Html.div [
+                                prop.className "flex items-center justify-center"
+                                prop.style [style.backgroundColor "gray"]
+                                prop.children [
+                                    Html.p [
+                                        prop.text 3
+                                    ]
+                                ]
+                            ]
+                            Html.div [
+                                prop.text 4
+                            ]
+                            Html.div [
+                                prop.className "justify-self-auto"
+                                prop.text 5
+                            ]
+                            Html.div [
+                                prop.className "justify-self-auto"
+                                prop.text 6
+                            ]
+                            Html.div [
+                                prop.className "justify-self-auto"
+                                prop.text 7
+                            ]
+                            Html.div [
+                                prop.className "justify-self-auto"
+                                prop.text 8
+                            ]
+                            Html.div [
+                                prop.className "justify-self-auto"
+                                prop.text 9
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+            (*
+            Html.div [
+                prop.className "flex flex-col items-center h-full"
                 prop.children [
                     Html.h1 [
                         prop.className "text-center text-5xl font-bold text-white mb-3 rounded-md p-4"
                         prop.text "NutritionCalculator"
+                        prop.style [ style.color "#16302B" ]
                     ]
                     ViewComponents.todoList model dispatch
                 ]
             ]
+            *)
         ]
     ]
